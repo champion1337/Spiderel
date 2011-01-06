4384445fa01c10ad464b85910e2aa67a541fd6c6
dapion <dan@dapion.com>
2011-01-06 17:48:16 +0200

    small fix

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__FILE__));
 
-$url = $_GET['url'];
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once (ROOT  . DS . 'bootstrap.php');
