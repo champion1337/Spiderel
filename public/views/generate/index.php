36de1ef3a7fcf2ef79c3fb7f9a124d6d37bce76a
dapion <dan@dapion.com>
2011-02-01 23:37:39 +0200

    added letter generator

--- public/views/generate/index.php
+++ public/views/generate/index.php
@@ -23,7 +23,9 @@ Ajax search code </br>
 <div class="search">
     <input type="text" name="search" id="search">
     <input type="button" name="go" id="go" value="search">
-</div>';
+</div>
+<div id="results"></div>
+';
     echo htmlentities( $code );
 ?>
 </textarea></br>
@@ -31,7 +33,40 @@ A-Z index code</br>
 <textarea>
 <?php
 
-$code = '<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+$code = '
+  <a href="#" class="letter">A</a>
+  <a href="#" class="letter">B</a>
+  <a href="#" class="letter">C</a>
+  <a href="#" class="letter">D</a>
+  <a href="#" class="letter">E</a>
+
+  <a href="#" class="letter">F</a>
+  <a href="#" class="letter">G</a>
+  <a href="#" class="letter">H</a>
+  <a href="#" class="letter">I</a>
+  <a href="#" class="letter">J</a>
+  <a href="#" class="letter">K</a>
+
+  <a href="#" class="letter">L</a>
+  <a href="#" class="letter">M</a>
+  <a href="#" class="letter">N</a>
+  <a href="#" class="letter">O</a>
+  <a href="#" class="letter">P</a>
+  <a href="#" class="letter">Q</a>
+
+  <a href="#" class="letter">R</a>
+  <a href="#" class="letter">S</a>
+  <a href="#" class="letter">T</a>
+  <a href="#" class="letter">U</a>
+  <a href="#" class="letter">V</a>
+  <a href="#" class="letter">W</a>
+
+  <a href="#" class="letter">X</a>
+  <a href="#" class="letter">Y</a>
+  <a href="#" class="letter">Z</a>
+
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script>
     $(document).ready(function() {
         $( ".letter" ).click(function() {
@@ -50,7 +85,9 @@ $code = '<script type="text/javascript" src="http://code.jquery.com/jquery-lates
             }
         });
     }
-    </script>';
+    </script>
+    <div id="results"></div>
+    ';
     echo htmlentities( $code );
     ?>
     </textarea>
